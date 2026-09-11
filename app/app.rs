cbe1ff1d61a069925461e453baad86ace4a77220
octobocto <bo@jalborg.com>
2026-09-11 14:49:36 -0700

    CLI: support independent wallet dir

--- app/app.rs
+++ app/app.rs
@@ -255,11 +255,7 @@ impl App {
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()?;
-        tracing::info!(
-            "Instantiating wallet with data directory: {}",
-            config.datadir.display()
-        );
-        let wallet = Wallet::new(&config.datadir.join("wallet.mdb"))?;
+        let wallet = Wallet::new(&config.wallet_dir.join("wallet.mdb"))?;
         if let Some(seed_phrase_path) = &config.mnemonic_seed_phrase_path {
             let mnemonic = std::fs::read_to_string(seed_phrase_path)?;
             let () = wallet.set_seed_from_mnemonic(mnemonic.as_str())?;
